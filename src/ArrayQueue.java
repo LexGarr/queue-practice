d2cd807fe95eed3e42b1a937da5f95f5236309c2
Cole Frederick <csp.frederick@gmail.com>
2016-09-08 14:50:07 -0600

    Commented and questions inserted!

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -2,10 +2,11 @@ import java.util.NoSuchElementException;
 
 public class ArrayQueue {
     private String[] items;
-    private long added;
-    private long taken;
+    private long added; // count of how many Strings have ever been added to this queue
+    private long taken; // ditto
     private boolean debug;
 
+    // this algorithm is interesting, let's make it easy to turn on debug printing!
     public ArrayQueue(boolean debug) {
         this.debug = debug;
         items = new String[8];
@@ -13,48 +14,61 @@ public class ArrayQueue {
     }
 
     public int count() {
-        return (int) (added - taken);
+        return (int) (added - taken); // makes sense, right?
     }
 
     public void enqueue(String s) {
-        if(debug) System.out.printf("enqueue: added %d taken %d\n", added, taken);
+        if(debug) System.out.printf("about to enqueue %4s: have added %d, taken %d, array length %d%n",
+                                    s, added, taken, items.length);
 
-        if(added - taken < items.length) {
+        if(count() < items.length) { // TODO what case does this represent?
             items[(int) (added % items.length)] = s;
             ++added;
-            return;
+            return; // done!
         }
 
+        // TODO what case does this represent?
+
         String[] a = new String[items.length * 2];
-        if(debug) System.out.println("Growing internal array to size: " + a.length);
+        if(debug) System.out.println("Growing internal array to size: " + a.length + "!!");
 
         for(long i = taken; i < added; ++i)
             a[(int) (i % a.length)] = items[(int) (i % items.length)];
+        // this goofy line translates item positions in the old array
+        // to their new homes in the new array
 
-        items = a;
-        enqueue(s);
+        items = a; // TODO what happens to the old array after we replace it with the new one?
+        enqueue(s); // TODO what the...? why are we recursing here?
     }
 
     public String dequeue() {
-        if(debug) System.out.printf("dequeue: added %d taken %d\n", added, taken);
+        if(count() == 0) throw new NoSuchElementException();
 
-        if(added > taken) {
-            String ret = items[(int) (taken % items.length)];
-            items[(int) (taken % items.length)] = null;
-            ++taken;
-            return ret;
-        }
-        throw new NoSuchElementException();
+        int front = (int) (taken % items.length);
+        String ret = items[front];
+        items[front] = null; // TODO why bother?
+
+        if(debug) System.out.printf("about to dequeue %4s: have added %d, taken %d, array length %d%n",
+                                    ret, added, taken, items.length);
+        ++taken;
+        return ret;
     }
+
+    // what is the size of the array if we add 10,000 strings, then remove 9,999 of them?
+    // TODO how should we fix this?
+    //   when should we shrink? when less than half full? how could that go wrong?
 
     public static void main(String[] args) {
         ArrayQueue q = new ArrayQueue(true);
         for(int i = 0; i < 6; ++i)
             q.enqueue(Integer.toBinaryString(i));
 
-        System.out.println(q.dequeue());
-        System.out.println(q.dequeue());
-        for(int i = 0; i < 6; ++i)
+        System.out.println();
+        System.out.println("q.dequeue() => " + q.dequeue());
+        System.out.println("q.dequeue() => " + q.dequeue());
+        System.out.println();
+
+        for(int i = 6; i < 12; ++i)
             q.enqueue(Integer.toBinaryString(i));
     }
 }
--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -1,10 +1,19 @@
 import java.util.NoSuchElementException;
+import java.util.Scanner;
 
+
+// simple linked implementation
+// it's generic, just like java.util.ArrayList
+
+// TODO can we make a LinkedQueue (or ArrayList) of ints? How can we get around this?
 public class LinkedQueue<E> {
-    private class Node<E> {
-        E data;
-        Node next;
-        Node(E data, Node next) {
+    // private inner class, also generic on type E (whatever type our queue is supposed to hold)
+    private static class Node<E> {
+        E data; // Nodes contain an E (the data)
+        Node<E> next;
+        // TODO these members aren't declared private
+        // ...can code outside the queue screw up our data structure now?
+        Node(E data, Node<E> next) {
             this.data = data; this.next = next;
         }
         Node(E data) {
@@ -29,7 +38,7 @@ public class LinkedQueue<E> {
         ++count;
         Node<E> newLast = new Node<E>(e);
 
-        if(first == null)
+        if(first == null) // TODO what does this case represent?
             first = last = newLast;
         else {
             last.next = newLast;
@@ -44,9 +53,42 @@ public class LinkedQueue<E> {
         E ret = first.data;
 
         first = first.next;
-        if(first == null)
+        if(first == null) // TODO what does this case represent?
             last = null;
 
         return ret;
+    }
+
+    // this implementation uses singly linked nodes to store its data
+    // it adds to the back and removes from the front
+
+    // TODO could we have done it backwards?
+    //   can we add to the front of a singly linked list?
+    //   can we remove from the back of a singly linked list?
+
+    public static void gatherInput(LinkedQueue<Double> q) {
+        // this is a try-with-resources block, it ensures the Scanner gets closed
+        try (Scanner kb = new Scanner(System.in)) {
+            System.out.print("Enter a floating point number: ");
+
+            while(kb.hasNextDouble()) {
+                double d = kb.nextDouble();
+                q.enqueue(d);
+                System.out.printf("Got %f, enter another: ", d);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // notice the diamond operator; it's shorthand for "new LinkedQueue<Double>()"
+        LinkedQueue<Double> arm_angles = new LinkedQueue<>();
+        gatherInput(arm_angles);
+
+        System.out.println("Input gathered, processing in FIFO (First in First Out) order...");
+
+        while(arm_angles.size() > 0) {
+            double degree = arm_angles.dequeue();
+            System.out.printf("Math.toRadians(%f) => %f\n", degree, Math.toRadians(degree));
+        }
     }
 }
